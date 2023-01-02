a78ee1ab4dedadbfe5729d4b94a1b35fb210a456
Abhinit Patil <abhineet0309@gmail.com>
2023-01-02 11:56:01 -0500

    REMOVED PORT From URL

--- index.js
+++ index.js
@@ -144,6 +144,6 @@ app.get("/search", async (req, res) => {
   res.send(results.hits.hits);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.listen(() => {
+  console.log(`Server listening`);
 });
